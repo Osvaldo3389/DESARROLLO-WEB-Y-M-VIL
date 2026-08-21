3c8ae3721bdd46de74db980d1b992b0c5476a777
gokudlg53 <minicarlosganador@gmail.com>
2026-08-21 18:33:28 -0400

    Update index.php

--- semana02/index.php
+++ semana02/index.php
@@ -3,6 +3,7 @@
         <title>Pagina Principal </title>
     </head>
      <body>
+         # se realiza la estructura basica de la pagina web
         <a href="empresa.php">Ir a Empresa</a><br>
         <a href="servicio.php">Ir a Servicios</a><br>
         <a href="productos.php">Ir a Productos</a><br>
