a7ebb7bb4382a12220ea44658ff2ea0a25ef6d28
Osvaldo3389 <oocaraccioli@gmail.com>
2026-08-21 18:27:16 -0400

    Add files via upload

---
+++ semana02/empresa.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Pagina Principal </title>
+    </head>
+     <body>
+        Hola Empresa<br>
+        <a href="empresa.php">Volver </a><br>
+      
+    </doby>
+</html>
---
+++ semana02/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Pagina Principal </title>
+    </head>
+     <body>
+        <a href="empresa.php">Ir a Empresa</a><br>
+        <a href="servicio.php">Ir a Servicios</a><br>
+        <a href="productos.php">Ir a Productos</a><br>
+        <a href="Contacto.php">Ir a Contacto</a><br>
+    </doby>
+</html>
