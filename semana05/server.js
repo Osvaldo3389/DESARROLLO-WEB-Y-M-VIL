fd3663ff42b41ec1d7efb1d0faf20c8dd1e68722
gokudlg53 <minicarlosganador@gmail.com>
2026-09-06 14:19:39 -0300

    Add files via upload

---
+++ semana05/server.js
@@ -0,0 +1,4 @@
+// se ocupan los modulos por defecto
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); // el cors valida los certificados de seguridad de los navegadores
